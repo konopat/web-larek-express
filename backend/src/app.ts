58b1ea807a01820827d3ecb0ce6d4ea5fbb767b6
konopat <k0n0pat@yandex.ru>
2025-06-07 18:14:40 +0800

    feat: connect mongoDB

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,22 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import mongoose from 'mongoose';
 
 dotenv.config();
 
 const { PORT } = process.env;
 const app = express();
 app.use(cors());
+
+// Подключение к MongoDB
+mongoose.connect('mongodb://127.0.0.1:27017/weblarek')
+  .then(() => {
+    console.log('Успешное подключение к MongoDB');
+  })
+  .catch((error) => {
+    console.error('Ошибка подключения к MongoDB:', error);
+  });
 
 app.listen(PORT, () => {
   console.log(`Ссылка на сервер: ${PORT}`);
