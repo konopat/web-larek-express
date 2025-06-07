f7b55c05d646689b943b4f5b7825a726a2c1aef4
konopat <k0n0pat@yandex.ru>
2025-06-07 18:00:09 +0800

    fix: cors problem

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 dotenv.config();
 
 const { PORT } = process.env;
 const app = express();
+app.use(cors());
 
 app.listen(PORT, () => {
   console.log(`Ссылка на сервер: ${PORT}`);
