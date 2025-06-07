1f0a37238cb7732c324d89d3e5042532b9d92900
konopat <k0n0pat@yandex.ru>
2025-06-07 18:43:06 +0800

    feat: add images

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
 import mongoose from 'mongoose';
+import path from 'path';
 import productRoutes from './routes/product';
 
 dotenv.config();
@@ -10,6 +11,7 @@ const { PORT } = process.env;
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public'))); // теперь клиент имеет доступ только к публичным файлам
 
 // Подключение роутов
 app.use('/product', productRoutes);
